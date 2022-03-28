05c7bfb9661125836691a94e5ae9585ff9f02554
Kutay Yaman <yamankutay1@gmail.com>
2022-03-28 04:32:56 +0300

    Some controls have been added to the Database's connect and disconnect methods

--- src/DatabaseConnection/Database.java
+++ src/DatabaseConnection/Database.java
@@ -8,22 +8,33 @@ public class Database {
     private final String jdbcURL = "jdbc:postgresql://localhost:5432/ChatApplication";
     private final String username = "postgres";
     private final String password = "admin";
-    private Connection connection;
+    private Connection connection = null;
 
-    public void connect(){
+    public void connect() {
         try {
-            connection = DriverManager.getConnection(jdbcURL, username, password);
-            System.out.println("Connected to PostgreSQL server");
+            if (connection == null) {
+                connection = DriverManager.getConnection(jdbcURL, username, password);
+                System.out.println("Connected to PostgreSQL server");
+            } else {
+                System.out.println("You have already established connection");
+            }
+
         } catch (SQLException e) {
             System.out.println("Error in connecting to PostgreSQL server");
             e.printStackTrace();
         }
     }
 
-    public void disconnect(){
+    public void disconnect() {
         try {
-            connection.close();
-            System.out.println("Disconnected from PostgreSQL server");
+            if (connection != null) {
+                connection.close();
+                connection = null;
+                System.out.println("Disconnected from PostgreSQL server");
+            } else {
+                System.out.println("You didn't already have an established connection");
+            }
+
         } catch (SQLException e) {
             System.out.println("An error occurred while terminating the connection. ");
             e.printStackTrace();
