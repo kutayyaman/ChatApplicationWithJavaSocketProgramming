552402e910c140e3e4455f4c929b77cdf7f9b213
Kutay Yaman <yamankutay1@gmail.com>
2022-04-18 04:43:20 +0300

    createAChat method of ChatRepository has been updated because i had added chatName column

--- src/Repository/ChatRepository.java
+++ src/Repository/ChatRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface ChatRepository {
     List<Chat> getAllByAccountIdWithMessages(Integer accountId);
 
-    Chat createAChat(List<User> users, User creatorUser);
+    Chat createAChat(List<User> users, User creatorUser, String chatName);
 }
--- src/Repository/Impl/ChatRepositoryImpl.java
+++ src/Repository/Impl/ChatRepositoryImpl.java
@@ -14,11 +14,11 @@ import java.util.List;
 public class ChatRepositoryImpl implements ChatRepository {
     @Override
     public List<Chat> getAllByAccountIdWithMessages(Integer accountId) {
-        String SQL = "Select c.id, m.id, m.body, m.sender_account_id, m.sender_user_name from Chat c " +
+        String SQL = "Select c.id, m.id, m.body, m.sender_account_id, m.sender_user_name, c.chat_name from Chat c " +
                 "INNER JOIN Account_Chat AC on c.id = AC.chat_id " +
                 "LEFT OUTER JOIN Message M on c.id = M.chat_id " +
                 "WHERE AC.account_id = ? " +
-                "ORDER BY m.id DESC";
+                "ORDER BY c.id DESC";
         List<Chat> result = new ArrayList<>();
         try {
             Connection connection = Database.connect();
@@ -36,14 +36,15 @@ public class ChatRepositoryImpl implements ChatRepository {
     }
 
     @Override
-    public Chat createAChat(List<User> users, User creatorUser) {
+    public Chat createAChat(List<User> users, User creatorUser, String chatName) {
         users.add(creatorUser);
-        String SQL = "INSERT INTO chat DEFAULT VALUES";
+        String SQL = "insert into chat(chat_name) values (?)";
         String accountChatSQL = "INSERT INTO account_chat(account_id, chat_id) VALUES (?,?)";
         Integer chatId = null;
         try {
             Connection connection = Database.connect();
             PreparedStatement pstmt = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, chatName);
             int affectedRows = pstmt.executeUpdate();
             if (affectedRows > 0) {
                 chatId = DatabaseUtils.getIdFromAddResult(pstmt);
@@ -75,15 +76,18 @@ public class ChatRepositoryImpl implements ChatRepository {
             String messageBody = rst.getString(3);
             Integer senderAccountId = rst.getInt(4);
             String senderUserName = rst.getString(5);
+            String chatName = rst.getString(6);
             Message message = new Message(messageId, messageBody, senderAccountId, chatIdFromDB,senderUserName);
 
             if (chat.getId() == null) {
                 chat.setId(chatIdFromDB);
+                chat.setChatName(chatName);
             }
             if (chat.getId() != chatIdFromDB) {
                 result.add(chat);
                 chat = new Chat();
                 chat.setId(chatIdFromDB);
+                chat.setChatName(chatName);
             }
             List<Message> messages = chat.getMessages();
             if (messages == null) {
